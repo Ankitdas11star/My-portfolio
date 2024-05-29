efc045e50f6401bf1a14986361231c71dcb9666d
Ankitdas11star <147165069+Ankitdas11star@users.noreply.github.com>
2024-05-29 23:54:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('header .navbar ul li a');
+  
+    navLinks.forEach(link => {
+      link.addEventListener('click', smoothScroll);
+    });
+  
+    function smoothScroll(event) {
+      event.preventDefault();
+      const targetId = event.currentTarget.getAttribute('href').substring(1);
+      const targetSection = document.getElementById(targetId);
+  
+      window.scrollTo({
+        top: targetSection.offsetTop,
+        behavior: 'smooth'
+      });
+    }
+  });
+  
